84603e9bdcc78e75c5802fc65dd0df96896b528e
YCRiver3038 <yuuki.cityriver@gmail.com>
2020-08-21 23:49:04 +0900

    Added averaging process

--- Arduino_Personal/VReg_PFM/VReg_PFM.ino
+++ Arduino_Personal/VReg_PFM/VReg_PFM.ino
@@ -2,13 +2,15 @@
 
 #define VREG_AIM_VOLTAGE 13.85f
 #define VREG_LIMIT_VOLTAGE 13.9
-#define AD2VO_COEF 0.0241f
+#define AD2VO_COEF 0.0235f
 #define AD2VO_OFFS (51.0174f)
 
 #define AD_Vo 2
 #define vDuty 9
 
 #define FLIPCTR_MAX (int16_t)32700
+#define AVG_bits 5
+#define AVG_SIZE uint8_t(1<<AVG_bits)
 
 void setup() 
 {
@@ -17,28 +19,46 @@ void setup()
 	int16_t flipTiming_u16 = 0;
 	int16_t flipctr = 0;
 	uint32_t ctrlStatus = 0;
-  uint8_t printctr = 0;
+	uint8_t printctr = 0;
+	uint8_t AVGCtr = 0;
+	uint16_t VAvg = 0;
+	uint8_t tempctr = 0;
+	uint16_t VRaw[AVG_SIZE] = {0};
 
-	FPID_GainSet gainUsing = {2.0f, 0.00002f, 0.04f, 0};
+	FPID_GainSet gainUsing = {1.2f, 0.00003f, 0.01f, 0};
 	FPIDController ctrl;
 	FPIDConfig cnfUsing(gainUsing);
+
 	pinMode(vDuty, OUTPUT);
+
 	digitalWrite(vDuty, LOW);
 	flipctr = 0;
 	ADCSRA = ADCSRA & 0b11111010;
-  //Serial.begin(115200);
+	Serial.begin(115200);
 
+	AVGCtr = 0;
 	while (1) 
 	{
-		readVo = (float)(analogRead(AD_Vo)) * AD2VO_COEF;
-    /*
-    if(printctr == 0)
-    {
-      Serial.println(readVo);
-    }
-    printctr++;
-    printctr%250;
-    */
+		VRaw[AVGCtr] = analogRead(AD_Vo);
+		while(tempctr < AVG_SIZE)
+		{
+			VAvg += VRaw[tempctr];
+			tempctr++;
+		}
+		tempctr = 0;
+		VAvg >>= AVG_bits;
+		AVGCtr = (AVGCtr+1)&(AVG_SIZE-1);
+		//Serial.println(AVGCtr);
+
+		readVo = (float)VAvg * AD2VO_COEF;
+		/*
+		if(printctr == 0)
+		{
+			Serial.println(readVo);
+		}
+		printctr++;
+		printctr%250;
+		*/
 		ctrl.control(cnfUsing, VREG_AIM_VOLTAGE, readVo, &flipTiming);
 		if(flipTiming > ((float)FLIPCTR_MAX-1.0f))
 		{
@@ -61,6 +81,7 @@ void setup()
 			digitalWrite(vDuty, LOW);
 			flipctr++;
 		}
+
 	}
 }
 
--- Arduino_Personal/VReg_PWM/VReg_PWM.ino
+++ Arduino_Personal/VReg_PWM/VReg_PWM.ino
@@ -1,37 +1,64 @@
 #include "FakePID.h"
 
 #define VREG_AIM_VOLTAGE 13.85f
-#define AD2VO_COEF 0.0241f
+#define AD2VO_COEF 0.0235f
 
 #define AD_Vo 2
 #define vDuty 9
 
+#define AVG_bits 5
+#define AVG_SIZE uint8_t(1<<AVG_bits)
+
 void setup() {
-  float readVo = 0.0;
-  float duty = 0.0;
+	float readVo = 0.0;
+	float duty = 0.0;
+	uint8_t AVGCtr = 0;
+	uint16_t VAvg = 0;
+	uint8_t tempctr = 0;
+	uint16_t VRaw[AVG_SIZE] = {0};
+	uint8_t printctr = 0;
 
-  FPID_GainSet gainUsing = {0.01f, 0.0001f, 0.01f, 0};
-  FPIDController ctrl;
-  FPIDConfig cnfUsing(gainUsing);
-  
-  TCCR1B = (TCCR1B & 0b11111000) | 0x01;
-  ADCSRA = ADCSRA & 0b11111010;
+	FPID_GainSet gainUsing = {0.8f, 0.00001f, 1.0f, 0};
+	FPIDController ctrl;
+	FPIDConfig cnfUsing(gainUsing);
+	Serial.begin(115200);
 
-  pinMode(vDuty, OUTPUT);
+	TCCR1B = (TCCR1B & 0b11111000) | 0x01;
+	ADCSRA = ADCSRA & 0b11111010;
 
-  while (1) {
-    readVo = (float)(analogRead(AD_Vo)) * AD2VO_COEF;
-    ctrl.control(cnfUsing, VREG_AIM_VOLTAGE, readVo, &duty);
-    if (duty >= 255.0f)
-    {
-      duty = 255.0f;
-    }
-    if (duty <= 0.0f)
-    {
-      duty = 0.0f;
-    }
-    analogWrite(vDuty, (uint8_t)duty);
-  }
+	pinMode(vDuty, OUTPUT);
+
+	while (1) {
+		VRaw[AVGCtr] = analogRead(AD_Vo);
+		while(tempctr < AVG_SIZE)
+		{
+			VAvg += VRaw[tempctr];
+			tempctr++;
+		}
+		tempctr = 0;
+		VAvg >>= AVG_bits;
+		AVGCtr = (AVGCtr+1)&(AVG_SIZE-1);
+		//Serial.println(AVGCtr);
+
+		readVo = (float)VAvg * AD2VO_COEF;
+		if(printctr == 0)
+		{
+			Serial.println(readVo);
+		}
+		printctr++;
+		printctr%250;
+		
+		ctrl.control(cnfUsing, VREG_AIM_VOLTAGE, readVo, &duty);
+		if (duty >= 255.0f)
+		{
+			duty = 255.0f;
+		}
+		if (duty <= 0.0f)
+		{
+			duty = 0.0f;
+		}
+		analogWrite(vDuty, (uint8_t)duty);
+	}
 }
 
 void loop() {}
